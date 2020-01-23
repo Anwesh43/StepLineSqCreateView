5b492efb9164c5bcfb479d785d09f116155633e2
Anwesh43 <anweshthecool0@gmail.com>
2020-01-24 03:43:31 +0530

    adding renderer in view

--- steplinesqbouncyview/src/main/java/com/anwesh/uiprojects/steplinesqbouncyview/StepLineSqBouncyView.kt
+++ steplinesqbouncyview/src/main/java/com/anwesh/uiprojects/steplinesqbouncyview/StepLineSqBouncyView.kt
@@ -68,15 +68,16 @@ class StepLineSqBouncyView(ctx : Context) : View(ctx) {
 
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -212,7 +213,7 @@ class StepLineSqBouncyView(ctx : Context) : View(ctx) {
             }
         }
 
-        fun handleTap(cb : () -> Unit) {
+        fun handleTap() {
             sbl.startUpdating {
                 animator.start()
             }
